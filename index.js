362e6b68997b750c41decf217159d4e34af0c0eb
zhipenglin <sunandmoon001@163.com>
2024-06-26 14:49:24 +0800

    修改管理关系执行时机

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ module.exports = fp(
   async (fastify, options) => {
     const config = merge({}, defaultConfig, options);
     const sequelize = new Sequelize(config.db);
+    const modelList = [];
     const addModels = async (modelsPath, options) => {
       const db = {},
         addModelsOptions = Object.assign({}, options);
@@ -37,34 +38,31 @@ module.exports = fp(
         { cwd: modelsPath }
       );
       const files = await glob(pattern, globOptions);
-      files
-        .map(file => {
-          const { name, model, associate, options } = require(path.join(modelsPath, file))({
-            sequelize,
-            DataTypes: Sequelize.DataTypes,
-            fastify,
-            options: config
-          });
-
-          const modelName = name || camelCase(path.basename(file, path.extname(file)));
-          db[modelName] = sequelize.define(
-            modelName,
-            model,
-            Object.assign(
-              {
-                paranoid: true,
-                tableName: (addModelsOptions.prefix || config.prefix || 't_') + snakeCase(modelName),
-                underscored: true
-              },
-              options
-            )
-          );
-          db[modelName].associate = associate;
-          return db[modelName];
-        })
-        .forEach(model => {
-          if (model.associate) model.associate(db);
+      files.map(file => {
+        const { name, model, associate, options } = require(path.join(modelsPath, file))({
+          sequelize,
+          DataTypes: Sequelize.DataTypes,
+          fastify,
+          options: config
         });
+
+        const modelName = name || camelCase(path.basename(file, path.extname(file)));
+        db[modelName] = sequelize.define(
+          modelName,
+          model,
+          Object.assign(
+            {
+              paranoid: true,
+              tableName: (addModelsOptions.prefix || config.prefix || 't_') + snakeCase(modelName),
+              underscored: true
+            },
+            options
+          )
+        );
+        db[modelName].associate = associate;
+        return db[modelName];
+      });
+      modelList.push(db);
       return db;
     };
     fastify.decorate('sequelize', {
@@ -73,6 +71,11 @@ module.exports = fp(
       [config.name || defaultConfig.name]: config.modelsPath && (await addModels(path.join(process.cwd(), config.modelsPath))),
       instance: sequelize,
       sync: async options => {
+        modelList.forEach(db => {
+          Object.values(db).forEach(model => {
+            if (model.associate) model.associate(db, fastify, options);
+          });
+        });
         await sequelize.sync(Object.assign({}, config.syncOptions, options));
         console.log('models were synchronized successfully.');
       }
